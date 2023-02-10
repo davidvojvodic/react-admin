dc63e460691d38414b87d19e0b07264eb7183469
David Vojvodić <davidvojvodic1@gmail.com>
2023-02-10 11:51:50 +0100

    prvi commit

---
+++ src/components/PieChart.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { ResponsivePie } from "@nivo/pie";
+import { useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import { mockPieData as data } from "../data/mockData";
+
+const PieChart = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <ResponsivePie
+      data={data}
+      theme={{
+        tooltip: {
+          container: {
+            color: "#000",
+          },
+        },
+      }}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      colors={{ scheme: "nivo" }}
+      borderWidth={1}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 0.2]],
+      }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor={colors.grey[100]}
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: "color" }}
+      enableArcLabels={false}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{
+        from: "color",
+        modifiers: [["darker", 2]],
+      }}
+      legends={[
+        {
+          anchor: "bottom",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: 56,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: colors.grey[100],
+          itemDirection: "left-to-right",
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: "circle",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemTextColor: "#000",
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
+
+export default PieChart;
